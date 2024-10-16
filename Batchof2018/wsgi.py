44ae76fd245877c6b8e4f8a858344c3d1f575506
UNOX <49128151+4TheKnight@users.noreply.github.com>
2024-10-16 13:53:28 -0700

    Update wsgi.py

--- Batchof2018/wsgi.py
+++ Batchof2018/wsgi.py
@@ -1,8 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Set the default settings module for the 'DJANGO_SETTINGS_MODULE' environment variable.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')  # Adjust this if your settings file is in a different module.
+# Set the default settings module for the 'DJANGO_SETTINGS_MODULE' environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Batchof2018.settings')
 
-# Get the WSGI application.
 application = get_wsgi_application()
