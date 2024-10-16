b61af52826ab0afe7424d5d4bb47aa552310a4c6
UNOX <49128151+4TheKnight@users.noreply.github.com>
2024-10-16 13:46:16 -0700

    Update wsgi.py

--- Batchof2018/wsgi.py
+++ Batchof2018/wsgi.py
@@ -1,13 +1,8 @@
 import os
-from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
-# Set the default Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Batchof2018.settings')
+# Set the default settings module for the 'DJANGO_SETTINGS_MODULE' environment variable.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')  # Adjust this if your settings file is in a different module.
 
-# Add the project path to the Python path
-project_path = Path(__file__).resolve().parent.parent
-if project_path not in os.sys.path:
-    os.sys.path.append(str(project_path))
-
+# Get the WSGI application.
 application = get_wsgi_application()
