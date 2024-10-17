267abe95eca43221b752ef8b50d5f51613a928da
UNOX <49128151+4TheKnight@users.noreply.github.com>
2024-10-17 04:47:19 -0700

    Update wsgi.py

--- Batchof2018/Main/wsgi.py
+++ Batchof2018/Main/wsgi.py
@@ -1,10 +1,11 @@
 import os
 import sys
-from django.core.wsgi import get_wsgi_application
 
-# Add the project root to the Python path
-project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(project_root)
+# Add the project root directory to the Python path
+project_root = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(project_root, 'Batchof2018'))
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Main.settings')
 
