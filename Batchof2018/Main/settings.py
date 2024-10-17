be55c1e854ed32120696354b07b4725267e20c78
UNOX <49128151+4TheKnight@users.noreply.github.com>
2024-10-17 03:46:59 -0700

    Update settings.py

--- Batchof2018/Main/settings.py
+++ Batchof2018/Main/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-swcebn#w5mtbg%e#g59msh52%wdlj+c3wm#fip3*duudzh#a(g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
